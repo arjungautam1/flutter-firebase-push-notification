f6b94710694fd069bddebeb57fe37349e4193bca
arjun-sudo <arjungautam5431@gmail.com>
2021-01-17 12:55:20 +0545

    model

---
+++ lib/model/message.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class Message {
+  final String title;
+  final String body;
+
+  const Message({
+    @required this.title,
+    @required this.body,
+  });
+}
